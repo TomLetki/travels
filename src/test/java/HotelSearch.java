a97742d48ca3bdb5f08027b8e41f989d5580f875
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-11-25 09:33:58 +0100

    Set amout of guests Search Check hotel list

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
@@ -23,17 +25,29 @@ public class HotelSearch {
       driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
       driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
 //data
-       //driver.findElement(By.name("checkin")).sendKeys("17/12/2021");
+       driver.findElement(By.name("checkin")).sendKeys("25/11/2021");
       // driver.findElement(By.name("checkout")).sendKeys("19/12/2021");
 
-       driver.findElement(By.name("checkin")).click();
+       driver.findElement(By.name("checkout")).click();
        driver.findElements(By.xpath("//td[@class='day 'and text()='30']"))
                .stream()
                .filter(el->el.isDisplayed())
                .findFirst()
                .ifPresent(el->el.click());
+        // driver.findElement(By.name("checkout")).sendKeys("19/12/2021");
+       // ilość gości
+       driver.findElement(By.id("travellersInput")).click();
+       driver.findElement(By.id("adultPlusBtn")).click();
+       driver.findElement(By.id("childPlusBtn")).click();
 
-       // driver.findElement(By.name("checkout")).sendKeys("19/12/2021");
+       driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
+       List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class, 'list_title')]//b")).stream()
+                                                                                .map(el->el.getText())
+                                                                                .collect(Collectors.toList());
+
+       System.out.println(hotelNames.size());
+
 
 
    }
