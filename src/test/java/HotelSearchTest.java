54b9d665316141a0aac697ff31a406162ce10556
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-12-08 09:38:49 +0100

    Redukcja kodu do 2 klas

--- src/test/java/HotelSearchTest.java
+++ src/test/java/HotelSearchTest.java
@@ -1,26 +1,22 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-public class HotelSearch {
 
+public class HotelSearchTest extends BaseTest{
 
+ @Test
 
-   @Test
-    public void searchHotel()  {
-       WebDriverManager.chromedriver().setup();
-       WebDriver driver = new ChromeDriver();
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       driver.manage().window().maximize();
-       driver.get("http://www.kurs-selenium.pl/demo/");
-//wybór miasta
+    public void searchHotelTest()  {
+    //wybór miasta
       //driver.findElement(By.xpath("//*[@id='s2id_autogen8']/a/span[1]")).click();
       driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
       driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
@@ -54,11 +50,31 @@ public class HotelSearch {
        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
 
-        // adding new User Account
+          }
+    @Test
+    public void searchHotelWithoutNameTest()  {
 
+//data
+        driver.findElement(By.name("checkin")).sendKeys("27/11/2021");
+        // driver.findElement(By.name("checkout")).sendKeys("19/12/2021");
 
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day 'and text()='30']"))
+                .stream()
+                .filter(el->el.isDisplayed())
+                .findFirst()
+                .ifPresent(el->el.click());
+        // driver.findElement(By.name("checkout")).sendKeys("19/12/2021");
+        // ilość gości
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
 
-   }
+        WebElement result = driver.findElement(By.xpath("//div[@class='itemscontainer']//h2")); //opcja Bartka
 
+        Assert.assertTrue(result.isDisplayed());
+        Assert.assertEquals("No Results Found", result.getText());
+
+    }
 
 }
--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -1,27 +1,25 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import net.bytebuddy.asm.Advice;
 import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-public class SignUpTest {
 
+public class SignUpTest extends BaseTest {
 
+  @Test
+    public void userSignUpTest() {
 
-   @Test
-    public void signUp()  {
-       WebDriverManager.chromedriver().setup();
-       WebDriver driver = new ChromeDriver();
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       driver.manage().window().maximize();
-       driver.get("http://www.kurs-selenium.pl/demo/");
 
        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed)
                                                                .findFirst().ifPresent(WebElement::click);
@@ -46,6 +44,51 @@ public class SignUpTest {
 
        
    }
+    @Test
+    public void userSignUpEmptyTest()  {
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed)
+                .findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        List<String> errors = driver.findElements(By.xpath("//div[@class='alert alert-danger']/p")).stream()
+                .map(WebElement::getText).collect(Collectors.toList());
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(errors.contains("The Email field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The First name field is required."));
+        softAssert.assertTrue(errors.contains("The Last Name field is required."));
+        softAssert.assertAll();
+
+    }
+    @Test
+    public void userSignUpWrongEmailTest()  {
+       driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed)
+                .findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        //uzupełnianie danych użytkownika
+        String lastname = "Comasz";
+        int randomNumber = (int) (Math.random()*1000);
+        String email = "Tester"+randomNumber+"$testy.pl";
+        driver.findElement(By.name("firstname")).sendKeys("Tomasz");
+        driver.findElement(By.name("lastname")).sendKeys("Comasz");
+        driver.findElement(By.name("phone")).sendKeys("+48555666777");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("Test123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        List<String> errors = driver.findElements(By.xpath("//div[@class='alert alert-danger']/p")).stream()
+                .map(WebElement::getText).collect(Collectors.toList());
+
+        Assert.assertTrue(errors.contains("The Email field must contain a valid email address."));
+
+    }
 
 
 }
