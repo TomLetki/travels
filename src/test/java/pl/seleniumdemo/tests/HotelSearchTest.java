2caa366b08c54bdd9e063f767c029c5949e0564e
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-12-15 13:47:21 +0100

    add first Page Object Add page factory

--- src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import pl.seleniumdemo.pages.HotelSearchPage;
 import pl.seleniumdemo.tests.BaseTest;
 
 import java.util.List;
@@ -16,26 +17,13 @@ public class HotelSearchTest extends BaseTest {
 
     public void searchHotelTest()  {
 
-      driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
-      driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
-      driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-//data
-       driver.findElement(By.name("checkin")).sendKeys("25/11/2021");
-      // driver.findElement(By.name("checkout")).sendKeys("19/12/2021");
+     HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+     hotelSearchPage.setCity("Dubai");
+     hotelSearchPage.setDates("22/12/2021", "24/12/2021");
+     hotelSearchPage.setTravellers();
+     hotelSearchPage.performSearch();
 
-       driver.findElement(By.name("checkout")).click();
-       driver.findElements(By.xpath("//td[@class='day 'and text()='30']"))
-                                                               .stream()
-                                                               .filter(el->el.isDisplayed())
-                                                               .findFirst()
-                                                               .ifPresent(el->el.click());
-        // driver.findElement(By.name("checkout")).sendKeys("19/12/2021");
-       // ilość gości
-       driver.findElement(By.id("travellersInput")).click();
-       driver.findElement(By.id("adultPlusBtn")).click();
-       driver.findElement(By.id("childPlusBtn")).click();
 
-       driver.findElement(By.xpath("//button[text()=' Search']")).click();
 
        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class, 'list_title')]//b")).stream()
                                                                                 .map(el->el.getAttribute("textContent"))
